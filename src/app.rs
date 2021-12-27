c49af9667aacbf3b68315a6a920e16de0f167c91
Johannes Fried <jcmf.schule@gmail.com>
2021-12-27 22:33:38 +0100

    Delete Unused Code and Increase Font Size

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,13 @@
-use eframe::{egui, epi};
+use eframe::{egui::{self, epaint::Fonts, FontDefinitions, DragValue, TextStyle}, epi};
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
     label: String,
-
-    // this how you opt-out of serialization of a member
-    #[cfg_attr(feature = "persistence", serde(skip))]
     value: f32,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
         }
@@ -24,35 +16,29 @@ impl Default for TemplateApp {
 
 impl epi::App for TemplateApp {
     fn name(&self) -> &str {
-        "eframe template"
+        "egui GDTF Inspector"
     }
 
-    /// Called once before the first frame.
-    fn setup(
-        &mut self,
-        _ctx: &egui::CtxRef,
-        _frame: &mut epi::Frame<'_>,
-        _storage: Option<&dyn epi::Storage>,
-    ) {
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        #[cfg(feature = "persistence")]
-        if let Some(storage) = _storage {
-            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
+    fn setup(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>, _storage: Option<&dyn epi::Storage>) {
+        // Increase font sizes
+        let mut fonts =  FontDefinitions::default();
+        for (key, (_family, size)) in fonts.family_and_size.iter_mut() {
+            match key {
+                TextStyle::Small => *size = 16.,
+                TextStyle::Body => *size = 20.,
+                TextStyle::Button => *size = 20.,
+                TextStyle::Heading => *size = 30.,
+                TextStyle::Monospace => *size = 20.,
+            }
         }
-    }
-
-    /// Called by the frame work to save state before shutdown.
-    /// Note that you must enable the `persistence` feature for this to work.
-    #[cfg(feature = "persistence")]
-    fn save(&mut self, storage: &mut dyn epi::Storage) {
-        epi::set_value(storage, epi::APP_KEY, self);
+        ctx.set_fonts(fonts);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        let Self { label, value } = self;
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
+        let Self { label: _, value } = self;
+
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -60,38 +46,19 @@ impl epi::App for TemplateApp {
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                egui::menu::menu(ui, "File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
-            });
-        });
+            ui.with_layout(egui::Layout::left_to_right(), |ui| {
+                if ui.button("Open").clicked() {
+                    println!("{}", "Open clicked")
+                };
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+                ui.label("Current File: ");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
+                ui.label("None");
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
+                if ui.button("🔄").clicked() {
+                    println!("{}", "Refresh clicked");
+                }
+            })
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -104,15 +71,12 @@ impl epi::App for TemplateApp {
                 "Source code."
             ));
             egui::warn_if_debug_build(ui);
-        });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
+            ui.add(DragValue::new(value));
+
+            ui.label(format!("{:#?}", ctx.fonts().definitions().family_and_size));
+            // println!("{:#?}", ctx.fonts().definitions().family_and_size);
+
+        });
     }
 }
